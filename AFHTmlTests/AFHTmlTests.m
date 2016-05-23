400e632a0e4abbddfbf023a6b4a2585be3555eaa
Amyr Farsad <amir.farsad@gmail.com>
2016-05-23 09:35:08 +0430

    Initial Commit

---
+++ AFHTmlTests/AFHTmlTests.m
@@ -0,0 +1,39 @@
+//
+//  AFHTmlTests.m
+//  AFHTmlTests
+//
+//  Created by Amir Farsad on 5/23/16.
+//  Copyright © 2016 Emersad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFHTmlTests : XCTestCase
+
+@end
+
+@implementation AFHTmlTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
